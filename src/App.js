a352143a1cdd4fde99f5f13052b477fb63e50817
Merisa Besirovic <besiroviccm@gmail.com>
2023-06-09 19:06:35 +0200

    created homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Pocetna from "./components/Pocetna/Pocetna";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Pocetna></Pocetna>
     </div>
   );
 }
---
+++ src/components/Pocetna/Pocetna.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Pocetna.css";
+
+export default function Pocetna() {
+  return (
+    <div className="pocetna-container">
+      <h1 className="pocetna-title">Obuća OBBE</h1>
+    </div>
+  );
+}
